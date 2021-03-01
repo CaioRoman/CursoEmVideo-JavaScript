ee5d150d3daa035c126442e80e008266c8d6b653
Caio Roman <caioroman7@gmail.com>
2021-03-01 20:31:42 -0300

    Aula11

---
+++ aula11/Ex-aula11A.js
@@ -0,0 +1,9 @@
+var vel = 70
+
+console.log(`A velocidade do seu carro é de ${vel}Km\h`)
+
+if(vel>60){
+    console.log(`Você ultrapaçou o limite de velocidade: MULTADO`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança `)
